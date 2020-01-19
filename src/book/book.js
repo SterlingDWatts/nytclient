6dabb91856925fcc0119ad05df1c2a7bcbbb07b5
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-01-19 10:55:06 -0800

    Create Book Component

---
+++ src/book/book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import moment from 'moment';
+import './book.css';
+
+export default function Book(props) {
+    return (
+        <div className='Book'>
+            <h2>{ props.title }</h2>
+            <div className='Book_author'>by { props.author }</div>
+            <div className='Book_publisher'>
+                Published by: { props.publisher }
+                on { moment(props.published_date).format('DD MMM YYYY') }
+            </div>
+            <div className='Book_description'>{ props.description }</div>
+            <div className='Book_details'>
+                Rank { props.rank } this week
+            </div>
+        </div>
+    );
+}
